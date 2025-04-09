5ffab3ca3230c7f0643cb12b2ddae5a11511b31d
LugiusXD <130033031+LugiusXD@users.noreply.github.com>
2025-04-09 11:13:54 +0200

    Update writescript.py

--- writescript.py
+++ writescript.py
@@ -5,7 +5,7 @@ import string
 # Change 'COM3' to the correct port for your Arduino (check in Arduino IDE)
 ser = serial.Serial('COM7', 9600, timeout=1)
 
-with open("button_log.txt", "a") as file:
+with open("times.txt", "a") as file:
     while True:
         #Might not be a good idea to skip errors
         line = ser.readline().decode("utf-8", errors="ignore").strip()
